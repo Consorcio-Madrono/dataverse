2e6d77f08065c9ac68e49ff555e1fb63e6e2f10a
Juan Corrales <juancorr@gmail.com>
2019-03-13 16:18:59 +0100

    updated #5624 to manage all facets

--- src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
@@ -36,18 +36,8 @@ public class DateUtil {
             formattedDate = formatDate(_date);
             return formattedDate;
         } catch (ParseException e) {
-            // Trick to evite the ParseException in French and Spanish Operating System installation
-            // Have the solr dateToDisplayOnCard parameter the date in a localized format?
-            try { 
-                format="dd-MMM-yyyy";
-                inputFormat = new SimpleDateFormat(format);
-                _date = inputFormat.parse(dateToformat);
-                formattedDate = formatDate(_date);
-                return formattedDate;
-            } catch (ParseException ex) {
-                ex.printStackTrace();
-                return null;
-            }
+            e.printStackTrace();
+            return null;
         }
     }
 
