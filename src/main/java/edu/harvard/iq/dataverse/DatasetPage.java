7afc35f3f38b989621d19dab4336d3b22ed585d0
juancorr <juancorr@gmail.com>
2025-12-03 12:05:13 +0100

    translating readme.txt

--- src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -504,10 +504,11 @@ public class DatasetPage implements java.io.Serializable {
     }
     
     // CSUC / MADROÑO BEGIN
-    public boolean uploadReadme (String token) {
+    public boolean uploadReadme (String token, String lang) {
       String uploadFileExec = System.getProperty("dataverse.path.uploadFile");
       String uploadFileDir  = System.getProperty("dataverse.files.directory") + "/temp/";
       String fqdn           = System.getProperty("dataverse.fqdn");
+      String langDirectory  = System.getProperty("dataverse.lang.directory");
       String siteUrl        = System.getProperty("dataverse.siteUrl");
       String resUrl         = siteUrl.replaceAll (java.util.regex.Pattern.quote("${dataverse.fqdn}"), fqdn); 
       String identifier     = dataset.getIdentifier();
@@ -517,9 +518,21 @@ public class DatasetPage implements java.io.Serializable {
       if (uploadFileExec==null || uploadFileExec.isEmpty()) {
           logger.warning("Warning, uploadFileExec null");
           return false;
+      } else if (uploadFileDir==null || uploadFileDir.isEmpty()) {
+          logger.warning("Warning, uploadFileDir null");
+          return false;
+      } else if (langDirectory==null || langDirectory.isEmpty()) {
+          logger.warning("Warning, langDirectory null");
+          return false;
+      } else if (fqdn==null || fqdn.isEmpty()) {
+          logger.warning("Warning, fqdn null");
+          return false;
+      } else if (siteUrl==null || siteUrl.isEmpty()) {
+          logger.warning("Warning, siteUrl null");
+          return false;
       }
       else if (new File(uploadFileExec).exists()) {
-          String uploadFileCmd []= {uploadFileExec, token, persistentId, uploadFileDir, resUrl, identifier};
+          String uploadFileCmd []= {uploadFileExec, token, persistentId, uploadFileDir, resUrl, identifier, langDirectory, lang};
           int exitValue;
           try {
               // Uploading readme.txt to the temp directory in the dataverse filesystem.
@@ -532,13 +545,13 @@ public class DatasetPage implements java.io.Serializable {
               for (FileMetadata fMetadata: getFileMetadatasSearch()) {
                 if (fileId==null) {
                     String name= fMetadata.getLabel();
-                    if (name.equals("readme.txt")) {
+                    if (name.equals("readme_"+ lang + ".txt")) {
                       fileId= fMetadata.getDataFile().getId();
                     }
                 }
               }
               String [] curlCommand= {"curl", "-H", "X-Dataverse-key:" + token, 
-                  "-X", "POST", "-F", "file=@" + uploadFileDir + identifier + "/readme.txt",
+                  "-X", "POST", "-F", "file=@" + uploadFileDir + identifier + "/readme_"+ lang + ".txt",
                   "-F", "jsonData={\"description\":\"ReadmeFile\",\"categories\":[\"Documentation\"]}",
                   resUrl + "/api/datasets/:persistentId/add?persistentId="+ persistentId
               }; // The curl comand is intended to create a new readme.txt file, but it cannot replace an existing one.
@@ -548,8 +561,7 @@ public class DatasetPage implements java.io.Serializable {
               process = runtime.exec(curlCommand);
               exitValue = process.waitFor();
 
-              if (exitValue== 0)
-                  init(); // Important. We will init the DatasetPage to get the configuration of the files and avoid failures.
+              PrimeFaces.current().executeScript("location.reload(true)");
 
           } catch (IOException | InterruptedException e) {
               logger.warning("Warning, IOException");
@@ -2840,9 +2852,8 @@ public class DatasetPage implements java.io.Serializable {
         return  returnToLatestVersion();
     }
 
-    public String submitDataset(String token) { // CSUC / MADROÑO Add the API token to generate the readme.txt
+    public String submitDataset() {
         try {
-            uploadReadme(token);  // CSUC / MADROÑO Call Function to generate and upload the Readme file.
             Command<Dataset> cmd = new SubmitDatasetForReviewCommand( dvRequestService.getDataverseRequest(), dataset);
             dataset = commandEngine.submit(cmd);
             //JsfHelper.addSuccessMessage(BundleUtil.getStringFromBundle("dataset.submit.success"));
@@ -4473,8 +4484,7 @@ public class DatasetPage implements java.io.Serializable {
         return false;
     }
 
-    public void processPublishButton(String token) { // CSUC / MADROÑO Add the API token to generate the readme.txt
-        uploadReadme(token);  // CSUC / MADROÑO Call Function to generate and upload the Readme file.
+    public void processPublishButton() {
         if (dataset.isReleased()) {
             PrimeFaces.current().executeScript("PF('publishDataset').show()");
         }
