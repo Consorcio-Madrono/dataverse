8316434d14f8c531c7c46b4e5e3ebeff12fb6881
juancorr <juancorr@gmail.com>
2025-12-16 19:49:09 +0100

    modifing json files

--- src/main/webapp/resources/js/dept.js
+++ src/main/webapp/resources/js/dept.js
@@ -1,61 +1,61 @@
 console.log("dept.js..");
-var rorSelector = "span[data-cvoc-protocol='ror']";
-var rorInputSelector = "input[data-cvoc-protocol='ror']";
-var rorRetrievalUrl = "https://api.ror.org/organizations";
-var rorIdStem = "https://ror.org/";
-var rorPrefix = "ror";
+var deptSelector = "span[data-cvoc-protocol='deptMadrono']";
+var deptInputSelector = "input[data-cvoc-protocol='deptMadrono']";
+var deptRetrievalUrl = "https://eciencia.consorciomadrono.es/department";
+var deptIdStem = "https://eciencia.consorciomadrono.es/";
+var deptPrefix = "deptMadrono";
 //Max chars that displays well for a child field
-var rorMaxLength = 31;
+var deptMaxLength = 63;
 
 $(document).ready(function() {
-    expandRors();
-    updateRorInputs();
+    expandDepts();
+    updateDeptInputs();
 });
 
-function expandRors() {
+function expandDepts() {
     // Check each selected element
-    $(rorSelector).each(function() {
-        var rorElement = this;
+    $(deptSelector).each(function() {
+        var deptElement = this;
         // If it hasn't already been processed
-        if (!$(rorElement).hasClass('expanded')) {
+        if (!$(deptElement).hasClass('expanded')) {
           //Child field case - if non-managed display, the string before this is name (affiliation) and we need to remove the duplicate affiliation string
           //This is true for Dataverse author field - may not be true elsewhere - tbd
-          let prev = $(rorElement)[0].previousSibling;
+          let prev = $(deptElement)[0].previousSibling;
           if(prev !== undefined) {
-          let val = $(rorElement)[0].previousSibling.nodeValue;
+          let val = $(deptElement)[0].previousSibling.nodeValue;
             if(val !== null) {
-              $(rorElement)[0].previousSibling.data = val.substring(0,val.indexOf('('));
+              $(deptElement)[0].previousSibling.data = val.substring(0,val.indexOf('('));
             }
           }
             // Mark it as processed
-            $(rorElement).addClass('expanded');
-            var id = rorElement.textContent;
-            if (!id.startsWith(rorIdStem)) {
-                $(rorElement).html(getRorDisplayHtml(id, null, ['No ROR Entry'], false, true));
+            $(deptElement).addClass('expanded');
+            var id = deptElement.textContent;
+            if (!id.startsWith(deptIdStem)) {
+                $(deptElement).html(getDeptDisplayHtml(id, null, ['No Entry'], false, true));
             } else {
-                //Remove the URL prefix - "https://ror.org/".length = 16
-                id = id.substring(rorIdStem.length);
+                //Remove the URL prefix - "https://eciencia.consorciomadrono.es/".length = 36
+                id = id.substring(deptIdStem.length);
                 //Check for cached entry
-                let value = getValue(rorPrefix, id);
+                let value = getValue(deptPrefix, id);
                 if(value.name !=null) {
-                    $(rorElement).html(getRorDisplayHtml(value.name, rorIdStem + id, value.altNames, false, true));
+                    $(deptElement).html(getDeptDisplayHtml(value.name, deptIdStem + id, value.altNames, false, true));
                 } else {
-                    // Try it as an ROR entry (could validate that it has the right form or can just let the GET fail)
+                    // Try it as an department of the Consorcio Madroño entry (could validate that it has the right form or can just let the GET fail)
                     $.ajax({
                         type: "GET",
-                        url: rorRetrievalUrl + "/" + id,
+                        url: deptRetrievalUrl + "/" + id,
                         dataType: 'json',
                         headers: {
                             'Accept': 'application/json',
                         },
-                        success: function(ror, status) {
+                        success: function(dept, status) {
                             // If found, construct the HTML for display
-                            var name = ror.name;
-                            var altNames= ror.acronyms;
+                            var name = dept.name;
+                            var altNames= dept.acronyms;
 
-                            $(rorElement).html(getRorDisplayHtml(name, rorIdStem + id, altNames, false, true));
+                            $(deptElement).html(getDeptDisplayHtml(name, deptIdStem + id, altNames, false, true));
                             //Store values in localStorage to avoid repeating calls to CrossRef
-                            storeValue(rorPrefix, id, name + "#" + altNames);
+                            storeValue(deptPrefix, id, name + "#" + altNames);
                         },
                         failure: function(jqXHR, textStatus, errorThrown) {
                             // Generic logging - don't need to do anything if 404 (leave
@@ -71,18 +71,18 @@ function expandRors() {
     });
 }
 
-function getRorDisplayHtml(name, url, altNames, truncate=true, addParens=false) {
+function getDeptDisplayHtml(name, url, altNames, truncate=true, addParens=false) {
     if(typeof(altNames) == 'undefined') {
         altNames=[];
     }
-    if (truncate && (name.length >= rorMaxLength)) {
+    if (truncate && (name.length >= deptMaxLength)) {
         // show the first characters of a long name
         // return item.text.substring(0,25) + "…";
         altNames.unshift(name);
-        name=name.substring(0,rorMaxLength) + "…";
+        name=name.substring(0,deptMaxLength) + "…";
     }
     if(url != null) {
-      name =  name + '<a href="' + url + '" target="_blank" rel="nofollow" >' +'<img alt="ROR logo" src="https://raw.githubusercontent.com/ror-community/ror-logos/main/ror-icon-rgb.svg" height="20" class="ror"/></a>';
+      name =  name + '<a href="' + url + '" target="_blank" rel="nofollow" >' +'<img alt="Consorcio Madroño logo" src="https://raw.githubusercontent.com/Consorcio-Madrono/dataverse/refs/heads/v6.5Madrono/src/main/webapp/resources/images/fav/favicon-32x32.png" height="20" class="ror"/></a>';
     }
     if(addParens) {
         name = ' (' + name + ')';
@@ -90,27 +90,27 @@ function getRorDisplayHtml(name, url, altNames, truncate=true, addParens=false) 
     return $('<span></span>').append(name).attr("title", altNames);
 }
 
-function updateRorInputs() {
-    // For each input element within rorInputSelector elements
-    $(rorInputSelector).each(function() {
-        var rorInput = this;
-        if (!rorInput.hasAttribute('data-ror')) {
+function updateDeptInputs() {
+    // For each input element within deptInputSelector elements
+    $(deptInputSelector).each(function() {
+        var deptInput = this;
+        if (!deptInput.hasAttribute('data-dept')) {
             // Random identifier
             let num = Math.floor(Math.random() * 100000000000);
-            // Hide the actual input and give it a data-ror number so we can
+            // Hide the actual input and give it a data-dept number so we can
             // find it
-            $(rorInput).hide();
-            $(rorInput).attr('data-ror', num);
+            $(deptInput).hide();
+            $(deptInput).attr('data-dept', num);
             // Todo: if not displayed, wait until it is to then create the
             // select 2 with a non-zero width
             // Add a select2 element to allow search and provide a list of
             // choices
-            var selectId = "rorAddSelect_" + num;
-            $(rorInput).after(
+            var selectId = "deptAddSelect_" + num;
+            $(deptInput).after(
                 '<select id=' + selectId + ' class="form-control add-resource select2" tabindex="0" >');
             $("#" + selectId).select2({
                 theme: "classic",
-                tags: $(rorInput).attr('data-cvoc-allowfreetext'),
+                tags: $(deptInput).attr('data-cvoc-allowfreetext'),
                 delay: 500,
                 templateResult: function(item) {
                     // No need to template the searching text
@@ -136,9 +136,9 @@ function updateRorInputs() {
                             altNames = idnum.substr(pos+2).split(',');
                             idnum=idnum.substr(0,pos);
                         }
-                        return getRorDisplayHtml(name, rorIdStem + idnum, altNames);
+                        return getDeptDisplayHtml(name, deptIdStem + idnum, altNames);
                     }
-                    return getRorDisplayHtml(name, null, ['No ROR Entry']);
+                    return getDeptDisplayHtml(name, null, ['No Entry']);
                 },
                 language: {
                     searching: function(params) {
@@ -146,12 +146,12 @@ function updateRorInputs() {
                         return 'Search by name or acronym…';
                     }
                 },
-                placeholder: rorInput.hasAttribute("data-cvoc-placeholder") ? $(rorInput).attr('data-cvoc-placeholder') : "Select a research organization",
+                placeholder: deptInput.hasAttribute("data-cvoc-placeholder") ? $(deptInput).attr('data-cvoc-placeholder') : "Select a research organization",
                 minimumInputLength: 3,
                 allowClear: true,
                 ajax: {
-                    // Use an ajax call to ROR to retrieve matching results
-                    url: rorRetrievalUrl,
+                    // Use an ajax call to the Consorcio Madroño Departments entry point to retrieve matching results
+                    url: deptRetrievalUrl,
                     data: function(params) {
                         term = params.term;
                         if (!term) {
@@ -178,11 +178,11 @@ function updateRorInputs() {
                                 // Prioritize those with this acronym
                                 .sort((a, b) => Number(b.acronyms.includes(params.term)) - Number(a.acronyms.includes(params.term)))
                                 // Prioritize previously used entries
-                                .sort((a, b) => Number(getValue(rorPrefix, b['id'].replace(rorIdStem,'')).name != null) - Number(getValue(rorPrefix, a['id'].replace(rorIdStem,'')).name != null))
+                                .sort((a, b) => Number(getValue(deptPrefix, b['id'].replace(deptIdStem,'')).name != null) - Number(getValue(deptPrefix, a['id'].replace(deptIdStem,'')).name != null))
                                 .map(
                                     function(x) {
                                         return {
-                                            text: x.name +", " + x.id.replace(rorIdStem,'') + ', ' + x.acronyms,
+                                            text: x.name +", " + x.id.replace(deptIdStem,'') + ', ' + x.acronyms,
                                             id: x.id
                                         }
                                     })
@@ -209,20 +209,20 @@ function updateRorInputs() {
 
             // If the input has a value already, format it the same way as if it
             // were a new selection
-            var id = $(rorInput).val();
-            if (id.startsWith(rorIdStem)) {
-                id = id.substring(rorIdStem.length);
+            var id = $(deptInput).val();
+            if (id.startsWith(deptIdStem)) {
+                id = id.substring(deptIdStem.length);
                 $.ajax({
                     type: "GET",
-                    url: rorRetrievalUrl + "/" + id,
+                    url: deptRetrievalUrl + "/" + id,
                     dataType: 'json',
                     headers: {
                         'Accept': 'application/json'
                     },
-                    success: function(ror, status) {
-                        var name = ror.name;
+                    success: function(dept, status) {
+                        var name = dept.name;
                         //Display the name and id number in the selection menu
-                        var text = name + ", " + ror.id.replace(rorIdStem,'') +', ' + ror.acronyms;
+                        var text = name + ", " + dept.id.replace(deptIdStem,'') +', ' + dept.acronyms;
                         var newOption = new Option(text, id, true, true);
                         $('#' + selectId).append(newOption).trigger('change');
                     },
@@ -233,9 +233,9 @@ function updateRorInputs() {
                     }
                 });
             } else {
-                // If the initial value is not in ROR, just display it as is
+                // If the initial value is not in the Consorcio Madroño departments entry point, just display it as is
                 var newOption = new Option(id, id, true, true);
-                newOption.altNames = ['No ROR Entry'];
+                newOption.altNames = ['No Entry'];
                 $('#' + selectId).append(newOption).trigger('change');
             }
             // Could start with the selection menu open
@@ -243,19 +243,19 @@ function updateRorInputs() {
             // When a selection is made, set the value of the hidden input field
             $('#' + selectId).on('select2:select', function(e) {
                 var data = e.params.data;
-                // For entries from ROR, the id and text are different
+                // For entries from the Consorcio Madroño departments, the id and text are different
                 //For plain text entries (legacy or if tags are allowed), they are the same
                 if (data.id != data.text) {
-                    // we want just the ror url
-                    $("input[data-ror='" + num + "']").val(data.id);
+                    // we want just the dept url
+                    $("input[data-dept='" + num + "']").val(data.id);
                 } else {
                     // Tags are allowed, so just enter the text as is
-                    $("input[data-ror='" + num + "']").val(data.id);
+                    $("input[data-dept='" + num + "']").val(data.id);
                 }
             });
             // When a selection is cleared, clear the hidden input
             $('#' + selectId).on('select2:clear', function(e) {
-                $("input[data-ror='" + num + "']").attr('value', '');
+                $("input[data-dept='" + num + "']").attr('value', '');
             });
             //When the field is selected via keyboard, move the focus and cursor to the new input
             $('#' + selectId).on('select2:open', function(e) {
